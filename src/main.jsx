16bbb2dd17a1d4bbd896a5ecbfde1bb35fa51fff
mateo <munozomateo@gmail.com>
2026-04-18 14:01:53 -0500

    Se implementa el provider del tema y el windwill en el main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Windmill} from "@windmill/react-ui"
+import { ThemeProvider } from './context/ThemeProvider'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider>
+            <Windmill usePreferences={false} theme={{mode: 'light'}}>
+                <App/>
+            </Windmill>
+        </ThemeProvider>
+    </StrictMode>,
 )
