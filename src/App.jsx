5bc58078d99920fef945655a8a80ca24cd6f50f7
mateo <munozomateo@gmail.com>
2026-04-18 13:59:52 -0500

    Se implementa manejo de rutas de react-router-dom y los providers en el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <>
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Layout from "./layout/Layout.jsx";
+import {SidebarProvider} from "./context/SidebarContext.jsx";
+import {ThemeProvider} from "./context/ThemeProvider.jsx";
 
-    </>
-  )
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <ThemeProvider>
+                <SidebarProvider>
+                    <Routes>
+                        <Route path={"/*"} element={<Layout/>}/>
+                    </Routes>
+                </SidebarProvider>
+                </ThemeProvider>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
