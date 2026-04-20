0c5112a521549d141c476491fd2feaa438ee234d
mateo <munozomateo@gmail.com>
2026-04-20 14:51:03 -0500

    IMplementación del authStore

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Layout from "./layout/Layout.jsx";
 import {SidebarProvider} from "./context/SidebarContext.jsx";
 import Home from "./layout/Home.jsx";
+import {useAuthStore} from "./store/authStore.js";
+import {useEffect} from "react";
 
 function App() {
+    const initAuth = useAuthStore(state => state.initAuth)
+
+    useEffect(() => {
+        initAuth()
+    }, [])
+
     return (
         <>
             <BrowserRouter>
