3d65ebfe1918203ae8c685be3ef9b98133ddafd5
mateo <munozomateo@gmail.com>
2026-04-19 22:05:16 -0500

    Se corrigen las rutas del sidebar

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -21,33 +21,6 @@ const routes = [
         icon: 'CategoriesIcon',
         name: 'Categorías',
     },
-    {
-        icon: 'PagesIcon',
-        name: 'Pages',
-        routes: [
-            // submenu
-            {
-                path: '/login',
-                name: 'Login',
-            },
-            {
-                path: '/create-account',
-                name: 'Create account',
-            },
-            {
-                path: '/forgot-password',
-                name: 'Forgot password',
-            },
-            {
-                path: '/app/404',
-                name: '404',
-            },
-            {
-                path: '/app/blank',
-                name: 'Blank',
-            },
-        ],
-    },
 ]
 
 export default routes
