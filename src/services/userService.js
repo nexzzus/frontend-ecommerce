82b55007a73eb01f2ba1b9fafc7e94f47b4f5ff6
mateo <munozomateo@gmail.com>
2026-04-20 14:01:29 -0500

    Implementación del axios configurado en el servicio de user

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,17 +1,15 @@
-import axios from "axios";
+import api from "../config/api.js";
 
-const API_URL = "http://localhost:8000/users"
+const API_URL = "/users"
 
 export const getUsers = async () => {
-    const response = await axios.get(API_URL);
+    const response = await api.get(API_URL);
     return response.data.data;
 }
 
 export const createUser = async (data) => {
     try {
-
-
-        const response = await axios.post(API_URL, data);
+        const response = await api.post(API_URL, data);
 
         if (response.status === 201) {
             return response.data.data;
@@ -31,7 +29,7 @@ export const createUser = async (data) => {
 
 export const deleteUser = async (id) => {
     try {
-        const response = await axios.delete(`${API_URL}/${id}`);
+        const response = await api.delete(`${API_URL}/${id}`);
         return response.data;
     } catch (error) {
         console.log("Error en el servicio", error);
@@ -47,7 +45,7 @@ export const deleteUser = async (id) => {
 
 export const updateUser = async (id, data) => {
     try {
-        const response = await axios.put(`${API_URL}/${id}/roles`, data)
+        const response = await api.put(`${API_URL}/${id}/roles`, data)
         return response.data.data;
     } catch (error) {
         const backendError = error.response?.data;
