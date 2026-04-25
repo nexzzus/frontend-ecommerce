63600c9a92c3f60a2b12eac5a8ff5906280b06d2
mateo <munozomateo@gmail.com>
2026-04-25 17:55:55 -0500

    servicio para endpoints de role

---
+++ src/services/rolesService.js
@@ -0,0 +1,56 @@
+import api from "../config/api.js";
+
+const API_URL = "/roles"
+
+export const getRoles = async () => {
+    try {
+        const response = await api.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        const backendError = error.response.data;
+
+        throw {
+            message: backendError.error.message || "Error desconocido",
+        }
+    }
+}
+
+export const createRoleService = async (data) => {
+    try {
+        const response = await api.post(API_URL, data);
+        return response.data;
+    }catch(error) {
+        console.log(error);
+        const backendError = error.response.data;
+        throw {
+            message: backendError.error.message || "Error desconocido",
+        }
+    }
+}
+
+export const updateRoleService = async (id, data) => {
+    try {
+        const response = await api.put(`${API_URL}/${id}`, data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        const backendError = error.response.data;
+        throw {
+            message: backendError.error.message || "Error desconocido",
+        }
+    }
+}
+
+export const deleteRoleService = async (id) => {
+    try {
+        const response = await api.delete(`${API_URL}/${id}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        const backendError = error.response.data;
+        throw {
+            message: backendError.error.message || "Error desconocido",
+        }
+    }
+}
