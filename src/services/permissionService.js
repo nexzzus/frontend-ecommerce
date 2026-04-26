2d0717326301c785e6d1976d093e71b790c2e9c6
mateo <munozomateo@gmail.com>
2026-04-25 22:30:14 -0500

    CRUD completo para los endpoints del servicio de permisos

--- src/services/permissionService.js
+++ src/services/permissionService.js
@@ -10,3 +10,30 @@ export const getPermissionsService = async () => {
         console.log(e)
     }
 }
+
+export const createPermissionService = async (data) => {
+    try {
+        const response = await api.post(API_URL, data)
+        return response.data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const deletePermissionService = async (id) => {
+    try {
+        const response = await api.delete(`${API_URL}/${id}`)
+        return response.data
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const editPermissionService = async (id, data) => {
+    try {
+        const response = await api.put(`${API_URL}/${id}`, data)
+        return response.data
+    } catch (e) {
+        console.log(e)
+    }
+}
